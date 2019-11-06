523229c35eb99e16300a490c07d00080c1b57592
Fülöp Dániel <fulopdaniel@gmail.com>
2019-11-06 17:52:38 +0100

    FociKlub osztályt

--- FociProject/FociProject/Models/FociKlub.cs
+++ FociProject/FociProject/Models/FociKlub.cs
@@ -8,5 +8,22 @@ namespace FociProject.Models
 {
     class FociKlub
     {
+        public DateTime Alapitas { get; set; }
+        public string Nev { get; set; }
+        public double Koltsegvetes { get; set; }
+        public List<Focista> Focistak { get; set; }
+
+        public FociKlub(DateTime alapitas, string nev, double koltsegvetes)
+        {
+            Alapitas = alapitas;
+            Nev = nev;
+            Koltsegvetes = koltsegvetes;
+            Focistak = new List<Focista>();
+
+        }
+
+        
+       
+
     }
 }
